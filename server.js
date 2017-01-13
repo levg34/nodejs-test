47f2f98a24881867f55589427ad97c5af9bd7db5
levg34 <guilhem.dross@laposte.net>
2017-01-13 14:46:05 +0100

    finished tutorial

--- server.js
+++ server.js
@@ -5,20 +5,9 @@ function start(route, handle) {
 	var port = 8888
 
 	function onRequest(request, response) {
-		var postData = ''
 		var pathname = url.parse(request.url).pathname
 		console.log('request for ' + pathname + ' received.')
-
-		request.setEncoding("utf8")
-
-		request.addListener("data", function (postDataChunk) {
-			postData += postDataChunk
-			console.log("Received POST data chunk '"+postDataChunk + "'.")
-		})
-
-		request.addListener("end", function () {
-			route(handle, pathname, response, postData)
-		})
+		route(handle, pathname, response, request)
 	}
 
 	http.createServer(onRequest).listen(port)
