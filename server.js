9f4eeeade626e4c85502f24c32f97cd38002d7ac
levg34 <guilhem.dross@laposte.net>
2017-01-13 13:36:00 +0100

    following tutorial p43

--- server.js
+++ server.js
@@ -1,15 +1,28 @@
-var http = require("http")
+var http = require('http')
+	var url = require('url')
 
-function start() {
+function start(route, handle) {
 	var port = 8888
 
-	http.createServer(function (request, response) {
-		console.log('request received.')
-		response.writeHead(200, {'Content-Type': 'text/plain'})
-		response.write('I love my wife.')
-		response.end()
-	}).listen(port)
+	function onRequest(request, response) {
+		var postData = ''
+		var pathname = url.parse(request.url).pathname
+		console.log('request for ' + pathname + ' received.')
 
+		request.setEncoding("utf8")
+
+		request.addListener("data", function (postDataChunk) {
+			postData += postDataChunk
+			console.log("Received POST data chunk '" +
+				postDataChunk + "'.")
+		})
+
+		request.addListener("end", function () {
+			route(handle, pathname, response, postData)
+		})
+	}
+
+	http.createServer(onRequest).listen(port)
 	console.log('server started on port ' + port)
 }
 
